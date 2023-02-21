fcb6ae3bedeb0963b88748df3665f35ad9bcf194
Anjan Shrestha <anjanshrestha@Anjans-MacBook-Air.local>
2023-02-22 02:16:36 +0545

    added day 4

--- src/Constant.java
+++
@@ -1,8 +0,0 @@
-public class Constant {
-    public static void main(String[] args) {
-        final double pie=3.14; // final keyword makes the value constant throught out the program.
-        int r=2;
-        double area=pie*r*r;
-        System.out.println("area of the circle with radius 2 is " +area);
-    }
-}
--- src/Intro.java
+++
@@ -1,5 +0,0 @@
-public class Intro {
-    public static void main(String[] arg){
-        System.out.println("My name is Anjan Shrestha.");
-    }
-}
