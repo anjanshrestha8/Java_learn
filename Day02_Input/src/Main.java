92682dc425bbaa9262482ba381db1553a22e0c50
Anjan Shrestha <anjanshrestha@Anjans-MacBook-Air.local>
2023-02-22 18:11:48 +0545

    added day 2 course

---
+++ Day02_Input/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] abc){
+
+        // System.out.println("Enter your name ");
+        // Scanner input= new Scanner(System.in);
+        // String name =input.nextLine();
+        // System.out.println("My name is "+name);
+        System.out.println("Enter the number");
+        Scanner input = new Scanner(System.in);
+        int a =input.nextInt();
+        int c=a*50;
+        System.out.println(c);
+
+    }
+}
