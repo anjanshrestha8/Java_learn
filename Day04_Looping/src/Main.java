89907bad14180b71694f022f6878e5795211eb58
Anjan Shrestha <anjanshrestha@Anjans-MacBook-Air.local>
2023-02-22 09:11:07 +0545

    added day 04 and src game

--- Day04_Looping/src/Main.java
+++ Day04_Looping/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // for loop
+//        int i=1;
+//        for(i=1;i<=100;i++){
+//            System.out.println("the loop run into:"+i+"times");
+//        }
+
+        //while loop
+        int i=1;
+        int money=1;
+        while(i<=10){
+            i++;
+            money*=i;
+        }
+        System.out.println(money);
     }
 }
