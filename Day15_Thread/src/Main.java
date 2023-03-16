5db3750437548db0bdd9c9213c43c88fd02a1b02
Anjan Shrestha <anjanshrestha@Anjans-MacBook-Air.local>
2023-03-16 11:31:09 +0545

    daily course updated

---
+++ Day15_Thread/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        Process1 p1 =new Process1();
+        Process2 p2 =new Process2();
+        p1.start();
+        p2.start();
+
+    }
+}
+class Process1 extends Thread{   // java class with thread
+
+    public void run(){
+        for(int i=0;i<=999999;i++){
+            System.out.println("process 1 is called.....");
+        }
+    }
+
+}
+class Process2 extends Thread{
+    @Override
+    public void run() {
+        for(int i=0;i<=999999;i++) {
+            System.out.println("process 2 is called.....");
+        }
+    }
+
+}
