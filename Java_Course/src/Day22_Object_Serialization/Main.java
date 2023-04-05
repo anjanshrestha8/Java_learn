0a230192fe116a8ba1f46d07387631a3afb0d5f6
Anjan Shrestha <anjanshrestha@Anjans-Air.mshome.net>
2023-04-05 11:10:25 +0545

    added daily course file handelling

--- Java_Course/src/Day22_Object_Serialization/Main.java
+++ Java_Course/src/Day22_Object_Serialization/Main.java
@@ -3,10 +3,22 @@ package Day22_Object_Serialization;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        FileOutputStream fileOutputtream = new FileOutputStream("src/Day22_Object_Serialization/anjan.txt");
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputtream);
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+//        Serialization
+        FileOutputStream fileOutputStream = new FileOutputStream("src/Day22_Object_Serialization/anjan.txt");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
         Person obj = new Person();
         objectOutputStream.writeObject(obj);
+
+//        Deserialization
+        FileInputStream fileInputStream = new FileInputStream("src/Day22_Object_Serialization/anjan.txt");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        Person obj1 =(Person) objectInputStream.readObject();
+        obj1.getAge();
+        obj1.getName();
+
+
+
     }
 }
---
+++ Java_Course/src/Day23_JDBC/Main.java
@@ -0,0 +1,16 @@
+package Day23_JDBC;
+
+public class Main {
+    public static void main(String[] args) {
+//        Class.forName("com.mysql.cj.jdbc.Driver");      // class import garxa ;
+
+
+//        String databaseUrl = "";
+
+
+//        Connection database
+
+//        Connection conn = DriverManager.getConnection();
+
+    }
+}
