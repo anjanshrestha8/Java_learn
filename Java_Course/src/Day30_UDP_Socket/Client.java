166c97e419bfea6cafa22d907962dcc21eee898b
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-05-15 12:17:56 +0545

    two way chat communication is established using UDP

--- Java_Course/src/Day30_UDP_Socket/Client.java
+++ Java_Course/src/Day30_UDP_Socket/Client.java
@@ -1,8 +1,23 @@
 package Day30_UDP_Socket;
 
-public class Client {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 
+public class Client {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Client Start");
+        DatagramSocket socket = new DatagramSocket(12345);
+        byte[] dataPacket = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(dataPacket,dataPacket.length);
+
+       while(true){
+           socket.receive(packet);
+           System.out.println(packet);
+           String recievePacket =new String(packet.getData(),0,packet.getLength());
+           System.out.println(recievePacket);
+       }
     }
 
 }
--- Java_Course/src/Day30_UDP_Socket/Server.java
+++ Java_Course/src/Day30_UDP_Socket/Server.java
@@ -1,7 +1,28 @@
 package Day30_UDP_Socket;
 
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // datagramSocket new socket is made
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress address = InetAddress.getByName("localhost");
+        //String message = "Hello World";
+
+        // sending packing via socket
+        Scanner input= new Scanner(System.in);
+
+     while(true){
+         String inputMessage = input.nextLine();
+         DatagramPacket packet = new DatagramPacket(inputMessage.getBytes(),inputMessage.length(),address,12345);
+
+         System.out.println("Sending Message......");
+         socket.send(packet);
+     }
+
 
 
     }
