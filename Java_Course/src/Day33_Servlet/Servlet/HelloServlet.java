3270344e13a790bb3164116e3bf9d8e99a53ae1f
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-06-06 09:36:50 +0545

    added applwt

--- Java_Course/src/Day33_Servlet/Servlet/HelloServlet.java
+++ Java_Course/src/Day33_Servlet/Servlet/HelloServlet.java
@@ -1,6 +1,19 @@
 package Day33_Servlet.Servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//"/hello"
+@WebServlet(urlPatterns = "/hello")   // jaba  "/hello" yo url ma add hunxa taba tala ko run hunxa
 
 public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        // this will run when browser return Method id "GET"
+        req.getRequestDispatcher("index.jsp").forward(req,res);
+    }
+
 }
